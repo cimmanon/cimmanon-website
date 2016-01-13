6f10003e2e50de835a4ecc5959dbe8dbcf51897d
cimmanon <chris@cimmanon.org>
2016-01-13 16:45:00 -0500

    Used cleaner method of removing nulls from the tag list (locks us into PostgreSQL 9.4, though)

--- sql/portfolio/component.sql
+++ sql/portfolio/component.sql
@@ -4,7 +4,7 @@ SELECT
 	date_added,
 	public,
 	archived,
-	COALESCE(NULLIF(array[NULL], array_agg(tag :: TEXT)), array[] :: TEXT[]) AS tags
+	COALESCE(array_agg(tag :: TEXT) FILTER (WHERE tag IS NOT NULL), array[] :: TEXT[]) AS tags
 FROM
 	portfolio.project_components
 	LEFT JOIN portfolio.project_tags USING (project, component, date_added)
