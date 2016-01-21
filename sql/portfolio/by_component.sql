e7b104f5afcc7f6db019db6232de768e922435f3
cimmanon <chris@cimmanon.org>
2016-01-20 22:45:40 -0500

    Adjusted query for tag/component listings to show less results (in general)

--- sql/portfolio/by_component.sql
+++ sql/portfolio/by_component.sql
@@ -1,3 +1,26 @@
+WITH
+	components AS (
+		SELECT
+			MAX(date_added) OVER (PARTITION BY project) AS last_update,
+			project,
+			component,
+			description,
+			date_added,
+			public,
+			archived,
+			array_agg(tag :: TEXT) AS tags,
+			row_number() OVER (PARTITION BY project, component ORDER BY date_added DESC) AS position
+		FROM
+			portfolio.project_components
+			JOIN portfolio.project_tags USING (project, component, date_added)
+		WHERE
+			lower(component) = ?
+			AND project_components.public = true
+		GROUP BY
+			project,
+			component,
+			date_added
+		)
 SELECT
 	project,
 	projects.description,
@@ -19,25 +42,12 @@ SELECT
 FROM
 	portfolio.projects
 	JOIN (
-		SELECT
-			MAX(date_added) OVER (PARTITION BY project) AS last_update,
-			project,
-			component,
-			description,
-			date_added,
-			public,
-			archived,
-			array_agg(tag :: TEXT) AS tags
-		FROM
-			portfolio.project_components
-			JOIN portfolio.project_tags USING (project, component, date_added)
-		WHERE
-			lower(component) = ?
-			AND project_components.public = true
-		GROUP BY
-			project,
-			component,
-			date_added) AS components USING (project)
+		-- select only the top components of each type for any given project
+		(SELECT * FROM components WHERE position = 1)
+		UNION ALL
+		-- select the top 10 from the remaining components
+		(SELECT * FROM components WHERE position > 1 ORDER BY position, last_update DESC, date_added DESC LIMIT 10)
+		) AS components USING (project)
 	LEFT JOIN portfolio.project_images USING (project, component, date_added)
 WHERE
 	project_images.featured = true OR project_images.featured IS NULL
--- sql/portfolio/by_tag.sql
+++ sql/portfolio/by_tag.sql
@@ -1,3 +1,38 @@
+WITH
+	-- we need to partition the components here to avoid situations where extremely popular tags
+	-- end up returning way too many results.  Ideally, no more than half of our total components
+	-- should be returned.
+	components AS (
+		SELECT
+			MAX(date_added) OVER (PARTITION BY project) AS last_update,
+			project,
+			component,
+			description,
+			date_added,
+			public,
+			archived,
+			array_agg(tag :: TEXT) AS tags,
+			row_number() OVER (PARTITION BY project, component ORDER BY date_added DESC) AS position
+		FROM
+			portfolio.project_components
+			JOIN portfolio.project_tags USING (project, component, date_added)
+			-- subquery here to avoid sequential scan
+			JOIN (
+				SELECT
+					project,
+					component,
+					date_added
+				FROM
+					portfolio.project_tags
+				WHERE
+					tag = ?
+				) AS has_tag USING (project, component, date_added)
+		WHERE
+			project_components.public = true
+		GROUP BY
+			project,
+			component,
+			date_added)
 SELECT
 	project,
 	projects.description,
@@ -19,35 +54,12 @@ SELECT
 FROM
 	portfolio.projects
 	JOIN (
-		SELECT
-			MAX(date_added) OVER (PARTITION BY project) AS last_update,
-			project,
-			component,
-			description,
-			date_added,
-			public,
-			archived,
-			array_agg(tag :: TEXT) AS tags
-		FROM
-			portfolio.project_components
-			JOIN portfolio.project_tags USING (project, component, date_added)
-			-- subquery here to avoid sequential scan
-			JOIN (
-				SELECT
-					project,
-					component,
-					date_added
-				FROM
-					portfolio.project_tags
-				WHERE
-					tag = ?
-				) AS has_tag USING (project, component, date_added)
-		WHERE
-			project_components.public = true
-		GROUP BY
-			project,
-			component,
-			date_added) AS components USING (project)
+		-- select only the top components of each type for any given project
+		(SELECT * FROM components WHERE position = 1)
+		UNION ALL
+		-- select the top 10 from the remaining components
+		(SELECT * FROM components WHERE position > 1 ORDER BY position, last_update DESC, date_added DESC LIMIT 10)
+		) AS components USING (project)
 	LEFT JOIN portfolio.project_images USING (project, component, date_added)
 WHERE
 	project_images.featured = true OR project_images.featured IS NULL
