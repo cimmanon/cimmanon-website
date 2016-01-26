add4f2ad75e730e2debe15372d3fed7173f33e1f
cimmanon <chris@cimmanon.org>
2016-01-26 18:06:24 -0500

    Fixed duplicate tags listed under the same category

--- sql/portfolio/tag_list.sql
+++ sql/portfolio/tag_list.sql
@@ -2,7 +2,7 @@ SELECT
 	category,
 	array_agg(tag :: TEXT ORDER BY tag) AS tags
 FROM
-	portfolio.project_type_tags
+	(SELECT DISTINCT ON (category, tag) * FROM portfolio.project_type_tags ORDER BY category, tag) AS x
 GROUP BY
 	category
 ORDER BY
